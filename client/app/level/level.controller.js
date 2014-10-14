f3e04a74c3af0b266c615095e67c7a1e0a5198cf
Emanuel Machado <wam-development@wam-development.(none)>
2014-10-14 18:51:39 +0100

    Done for the day... Not.

--- client/app/level/level.controller.js
+++ client/app/level/level.controller.js
@@ -44,86 +44,58 @@ angular.module('fruitGameApp')
   				sound: 'someAudio.mp3'
   			}
   		};
-
-  		// Generate fruit array
-  		function generateFruits (length) {
-  			$scope.currentFruits = [];
-  			for(var i = 0; i < length; i++) {
-  				$scope.currentFruits.push($scope.fruits[Object.keys($scope.fruits)[(Math.floor(Math.random() * (5 - 0)) + 0)]]);
-  			}
-  		}
-
-  		if ( $scope.level < 4 ) {
-  			generateFruits(3);
-  		}
-  		else {
-  			if ( $scope.level < 10 ) {
-  				generateFruits(5);
-  			}
-  			else {
-  				generateFruits(8);
-  			}
-  		}
 		
 
-  		// Handle keypress 
-  		var timeDiff, currentInput, lastInput;
-  		var inputIndex = 0;
-  		var isPressing = false;
-    	function detectKey(event){
-    		isPressing = true;
-		      switch(event.which)
-		      {         
-		          case 97: //A
-		              $scope.activeFruit = $scope.fruits.orange;
-		              break;
-		           
-		          case 115: //S
-		              $scope.activeFruit = $scope.fruits.banana;
-		              break;
-		           
-		          case 100: //D
-		              $scope.activeFruit = $scope.fruits.kiwi;
-		              break;
-		           
-		          case 102: //F
-		              $scope.activeFruit = $scope.fruits.strawberry;
-		              break;
-		               
-		          case 103: //G
-		              $scope.activeFruit = $scope.fruits.other;
-		              break;
-		      }
+	// Handle keypress 
+	var timeDiff, currentInput, lastInput;
+	var inputIndex = 0;
+	var isPressing = false;
+	function detectKey(event){
+		isPressing = true;
+	      switch(event.which)
+	      {         
+	          case 97: //A
+	              $scope.activeFruit = $scope.fruits.orange;
+	              break;
+	           
+	          case 115: //S
+	              $scope.activeFruit = $scope.fruits.banana;
+	              break;
+	           
+	          case 100: //D
+	              $scope.activeFruit = $scope.fruits.kiwi;
+	              break;
+	           
+	          case 102: //F
+	              $scope.activeFruit = $scope.fruits.strawberry;
+	              break;
+	               
+	          case 103: //G
+	              $scope.activeFruit = $scope.fruits.other;
+	              break;
+	      }
 
-		    // Check if the pressed fruit is correct
-		    if( $scope.activeFruit === $scope.currentFruits[inputIndex] ) {
-		    	currentInput = new Date();
-			    timeDiff = currentInput.valueOf() - lastInput.valueOf();
-			    lastInput = new Date();
-			    $scope.score += $scope.level * 5 + Math.floor(20000/timeDiff);
-			    inputIndex++;
-			    $scope.$apply();
-			    if ( $scope.currentFruits.length === inputIndex ) {
-			    	alert('Ganhaste, crl! És bem fino, bro');
-			    }
+	    // Check if the pressed fruit is correct
+	    if( $scope.activeFruit === $scope.currentFruits[inputIndex] ) {
+	    	currentInput = new Date();
+		    timeDiff = currentInput.valueOf() - lastInput.valueOf();
+		    lastInput = new Date();
+		    $scope.score += $scope.level * 5 + Math.floor(20000/timeDiff);
+		    inputIndex++;
+		    $scope.$apply();
+		    if ( $scope.currentFruits.length === inputIndex ) {
+		    	alert('Ganhaste, crl! És bem fino, bro');
+		    	$scope.level++;
+		    	newLevel();
 		    }
-		    else {
-		    	alert('És burro, pah??');
-		    }
-	  }
+	    }
+	    else {
+	    	alert('És burro, pah??');
+	    }
+	}
 
-  		var index = 0;
-		function changeFruit(i) {
-			$scope.activeFruit = $scope.currentFruits[i];
-			index++;
-		}
-
-		// Handle the sequence
-		changeFruit(index);
-		$interval(function () {
-			changeFruit(index);
-		}, 1500/$scope.level, $scope.currentFruits.length);
-
+	
+	// Start playing
 	function startGame() {
 		lastInput = new Date();
 		document.onkeypress = 	function () {
@@ -135,5 +107,41 @@ angular.module('fruitGameApp')
 	  							};
 	}
 
-	startGame();
+	function newLevel() {
+		// Generate fruit array
+		function generateFruits (length) {
+			$scope.currentFruits = [];
+			for(var i = 0; i < length; i++) {
+				$scope.currentFruits.push($scope.fruits[Object.keys($scope.fruits)[(Math.floor(Math.random() * (5 - 0)) + 0)]]);
+			}
+		}
+
+		if ( $scope.level < 4 ) {
+			generateFruits(3);
+		}
+		else {
+			if ( $scope.level < 10 ) {
+				generateFruits(5);
+			}
+			else {
+				generateFruits(8);
+			}
+		}  	
+
+		// Handle the sequence
+		var index = 0;
+		function changeFruit(i) {
+			$scope.activeFruit = $scope.currentFruits[i];
+			index++;
+		}
+		
+		changeFruit(index);
+		$interval(function () {
+			changeFruit(index);
+		}, 1500/$scope.level, $scope.currentFruits.length);
+		startGame();
+	}
+
+	newLevel();
+
   });
