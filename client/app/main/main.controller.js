9d80d162abea75d61eca961c2a859b84a27d17ed
Emanuel Machado <wam-development@wam-development.(none)>
2014-10-14 12:38:19 +0100

    Main screen and key bindings

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('fruitGameApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.awesomeThings = [];
+    /*$scope.awesomeThings = [];
 
     $http.get('/api/things').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
@@ -23,5 +23,33 @@ angular.module('fruitGameApp')
 
     $scope.$on('$destroy', function () {
       socket.unsyncUpdates('thing');
-    });
+    });*/
+
+    function detectKey(event){
+
+      switch(event.which)
+      {         
+          case 97: //A
+              console.log("A");
+              break;
+           
+          case 115: //S
+              console.log("S");
+              break;
+           
+          case 100: //D
+              console.log("D");
+              break;
+           
+          case 102: //F
+              console.log("F");
+              break;
+               
+          case 103: //G
+              console.log("G");
+              break;
+      }
+  }
+  document.onkeypress=detectKey;
+
   });
