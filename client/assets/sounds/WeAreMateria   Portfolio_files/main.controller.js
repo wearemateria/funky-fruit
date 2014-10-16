7de79f4d5f94789f63b7defb59be4328ac9d2421
ECMachado <wam-development@wam-development.(none)>
2014-10-16 20:08:20 +0100

    logo

---
+++ client/assets/sounds/WeAreMateria   Portfolio_files/main.controller.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('portfolioApp')
+  .controller('MainCtrl', function ($scope, $http, socket, $rootScope) {
+    $scope.awesomeThings = [];
+
+    $http.get('/api/things').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+      socket.syncUpdates('thing', $scope.awesomeThings);
+    });
+
+    $scope.addThing = function() {
+      if($scope.newThing === '') {
+        return;
+      }
+      $http.post('/api/things', { name: $scope.newThing });
+      $scope.newThing = '';
+    };
+
+    $scope.deleteThing = function(thing) {
+      $http.delete('/api/things/' + thing._id);
+    };
+
+    $scope.$on('$destroy', function () {
+      socket.unsyncUpdates('thing');
+    });
+
+    $http.get('http://m.wearemateria.info/projects')
+      .success(function (data) {
+        $rootScope.projects = data;
+      });
+  });
